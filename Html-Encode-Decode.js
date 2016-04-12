c2b2fd2a6c88aefed42c2222ef96f1fe87bb594f
aimwhy <aimwhy@foxmail.com>
2016-04-12 23:46:53 +0800

    Update Html-Encode-Decode.js

--- Html-Encode-Decode.js
+++ Html-Encode-Decode.js
@@ -3,11 +3,11 @@ function encode(string) {
 	textarea = document.createElement('textarea');
 	textarea.textContent = string;
 	return textarea.innerHTML;
-};
+}
 
 function decode(string) {
 	var textarea;
 	textarea = document.createElement('textarea');
 	textarea.innerHTML = string;
 	return textarea.textContent;
-};
+}
